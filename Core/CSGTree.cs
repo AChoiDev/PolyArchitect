f7f6ce4fd7358d2163b65c1a6154730037d53bfb
MrDiamondGold <rafeohall@gmail.com>
2023-11-19 19:07:36 -0800

    Formatting and BrushBuilder.cs fixes

--- Core/CSGTree.cs
+++ Core/CSGTree.cs
@@ -4,91 +4,89 @@ using System.Linq;
 
 namespace PolyArchitect.Core {
 
-public enum BooleanOperation {Add, Subtract, Intersect}
+    public enum BooleanOperation { Add, Subtract, Intersect }
 
-// A tree of CSG shapes
-// TODO: check all leaves of trees are brushes
-// and check all non-leaves are non brushes
-// and infer subset tree from intersecting brushes
-public class CSGTree {
-    private readonly List<CSGTree> subTrees;
-    public string Name {get; private set;}
+    // A tree of CSG shapes
+    // TODO: check all leaves of trees are brushes
+    // and check all non-leaves are non brushes
+    // and infer subset tree from intersecting brushes
+    public class CSGTree {
+        private readonly List<CSGTree> subTrees;
+        public string Name { get; private set; }
 
-    public BooleanOperation operation;
-    private readonly Brush brush;
-    
+        public BooleanOperation operation;
+        private readonly Brush brush;
 
-    public CSGTree(BooleanOperation operation, string name, Brush brush = null) {
-        this.operation = operation;
-        subTrees = new();
-        this.Name = name;
-        this.brush = brush;
-    }
 
-    public PolygonSide Categorize(ConvexPolygon polygon) {
-        if (brush != null) {
-            return brush.Categorize(polygon);
+        public CSGTree(BooleanOperation operation, string name, Brush brush = null) {
+            this.operation = operation;
+            subTrees = new();
+            this.Name = name;
+            this.brush = brush;
         }
 
-        var compositeCategory = PolygonSide.OUTSIDE;
-
-        foreach (var subtree in subTrees) {
-            var subCategory = subtree.Categorize(polygon);
-            switch (subtree.operation)
-            {
-                case BooleanOperation.Add:
-                    compositeCategory = CategorizationUtilities.AddCategorize(compositeCategory, subCategory);
-                    break;
-                case BooleanOperation.Subtract:
-                    compositeCategory = CategorizationUtilities.SubtractCategorize(compositeCategory, subCategory);
-                    break;
-                case BooleanOperation.Intersect:
-                    compositeCategory = CategorizationUtilities.IntersectCategorize(compositeCategory, subCategory);
-                    break;
+        public PolygonSide Categorize(ConvexPolygon polygon) {
+            if (brush != null) {
+                return brush.Categorize(polygon);
             }
+
+            var compositeCategory = PolygonSide.OUTSIDE;
+
+            foreach (var subtree in subTrees) {
+                var subCategory = subtree.Categorize(polygon);
+                switch (subtree.operation) {
+                    case BooleanOperation.Add:
+                        compositeCategory = CategorizationUtilities.AddCategorize(compositeCategory, subCategory);
+                        break;
+                    case BooleanOperation.Subtract:
+                        compositeCategory = CategorizationUtilities.SubtractCategorize(compositeCategory, subCategory);
+                        break;
+                    case BooleanOperation.Intersect:
+                        compositeCategory = CategorizationUtilities.IntersectCategorize(compositeCategory, subCategory);
+                        break;
+                }
+            }
+
+            return compositeCategory;
+
         }
 
-        return compositeCategory;
+        public List<Brush> GetBrushes() {
+            var brushes = new List<Brush>();
+            if (brush != null) {
+                brushes.Add(brush);
+            }
 
-    }
+            foreach (var subtree in subTrees) {
+                brushes.AddRange(subtree.GetBrushes());
+            }
 
-    public List<Brush> GetBrushes() {
-        var brushes = new List<Brush>();
-        if (brush != null) {
-            brushes.Add(brush);
+            return brushes;
         }
 
-        foreach (var subtree in subTrees) {
-            brushes.AddRange(subtree.GetBrushes());
+        public void AddTree(CSGTree subTree) {
+            subTrees.Add(subTree);
         }
 
-        return brushes;
-    }
-
-    public void AddTree(CSGTree subTree) {
-        subTrees.Add(subTree);
-    }
-
-    public static string opStr(BooleanOperation? operation) {
-        return operation switch
-        {
-            (BooleanOperation.Add) => "+ ",
-            (BooleanOperation.Subtract) => "- ",
-            (BooleanOperation.Intersect) => "∩ ",
-            _ => "",
-        };
-    }
-
-    public string Print(int level) {
-        var opName = Enum.GetName(typeof(BooleanOperation), operation);
-        var lineStr = string.Join("", Enumerable.Range(0, level).Select((i) => "|>> ")) + opStr(operation) + Name;
-
-        foreach (var tree in subTrees) {
-            lineStr += "\n";
-            lineStr += tree.Print(level + 1);
+        public static string opStr(BooleanOperation? operation) {
+            return operation switch {
+                (BooleanOperation.Add) => "+ ",
+                (BooleanOperation.Subtract) => "- ",
+                (BooleanOperation.Intersect) => "∩ ",
+                _ => "",
+            };
         }
 
-        return lineStr;
+        public string Print(int level) {
+            var opName = Enum.GetName(typeof(BooleanOperation), operation);
+            var lineStr = string.Join("", Enumerable.Range(0, level).Select((i) => "|>> ")) + opStr(operation) + Name;
+
+            foreach (var tree in subTrees) {
+                lineStr += "\n";
+                lineStr += tree.Print(level + 1);
+            }
+
+            return lineStr;
+        }
     }
-}
 }
--- PAUnity/PABrush.cs
+++ PAUnity/PABrush.cs
@@ -1,37 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using PolyArchitect.Core;
 using UnityEngine;
 
 namespace PolyArchitect.Unity {
 
-// component wrapper for core brush 
-[ExecuteInEditMode()]
-public class PABrush : MonoBehaviour
-{
+    // component wrapper for core brush 
+    [ExecuteInEditMode()]
+    public class PABrush : MonoBehaviour {
+        // NOTE: Temporary enum to control the type of brush generated
+        public enum BrushType {
+            Cylinder,
+            Box,
+            Cone,
+        }
 
-    public BooleanOperation operation;
-    private Core.Brush brush = null;
+        // NOTE: Temporary variable to control the type of brush generated
+        public BrushType type;
 
-    private Matrix4x4? preMatrixTransform = null;
+        public BooleanOperation operation;
+        private Core.Brush brush = null;
+
+        private Matrix4x4? preMatrixTransform = null;
 
 
-    public void OnDrawGizmos() {
-        if (brush != null) {
-            if (transform.localToWorldMatrix != preMatrixTransform) {
-                brush.ChangeTransform(Convert.Transform(transform));
+        public void OnDrawGizmos() {
+            if (brush != null) {
+                if (transform.localToWorldMatrix != preMatrixTransform) {
+                    brush.ChangeTransform(Convert.Transform(transform));
+                    preMatrixTransform = transform.localToWorldMatrix;
+                }
+            } else {
+                switch (type) {
+                    case BrushType.Cylinder:
+                        brush = Core.Brush.MakeCylinder(6, transform);
+                        break;
+                    case BrushType.Box:
+                        brush = Core.Brush.MakeBox(transform);
+                        break;
+                    case BrushType.Cone:
+                        brush = Core.Brush.MakeCone(6, transform);
+                        break;
+                    default:
+                        break;
+                }
                 preMatrixTransform = transform.localToWorldMatrix;
             }
-        } else {
-            brush = Core.Brush.MakeCylinder(6, transform);
-            preMatrixTransform = transform.localToWorldMatrix;
         }
-    }
 
-    public Brush GetBrush()
-    {
-        return brush;
-    }
+        public Brush GetBrush() {
+            return brush;
+        }
 
-}
+    }
 }
